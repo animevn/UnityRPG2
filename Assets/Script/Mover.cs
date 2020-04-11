7af2858e347c47458274cc80886f36d159842a15
animevn <haanhth21@gmail.com>
2020-04-11 13:35:10 +0700

    Part 5 ok now

--- Assets/Script/Mover.cs
+++ Assets/Script/Mover.cs
@@ -4,13 +4,13 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-    
     Ray rayLight;
         
     void Update()
     {
         if (Input.GetMouseButtonDown(0)){
             rayLight = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
         }
         Debug.DrawRay(rayLight.origin, rayLight.direction * 1000);
         GetComponent<NavMeshAgent>().destination = target.position;
